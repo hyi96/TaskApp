686635559755700c423dcb6248d5415e00db0bfc
hyi96 <yihao0906@gmail.com>
2026-04-04 05:06:53 -0700

    add burntToast

--- TaskApp/Services/NotificationService.cs
+++ TaskApp/Services/NotificationService.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Diagnostics;
-using System.Security;
-using System.Text;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace TaskApp.Services;
 
@@ -14,39 +12,14 @@ public static class NotificationService
 
         try
         {
-            ShowWindowsToast(title, body);
+            new ToastContentBuilder()
+                .AddText(title)
+                .AddText(body)
+                .Show();
         }
         catch
         {
             // Notification is best-effort; never crash the app
         }
-    }
-
-    private static void ShowWindowsToast(string title, string body)
-    {
-        var escapedTitle = SecurityElement.Escape(title) ?? title;
-        var escapedBody = SecurityElement.Escape(body) ?? body;
-
-        var script =
-            "$null=[Windows.UI.Notifications.ToastNotificationManager,Windows.UI.Notifications,ContentType=WindowsRuntime]\n" +
-            "$null=[Windows.Data.Xml.Dom.XmlDocument,Windows.Data.Xml.Dom.XmlDocument,ContentType=WindowsRuntime]\n" +
-            "$x=New-Object Windows.Data.Xml.Dom.XmlDocument\n" +
-            "$x.LoadXml('<toast><visual><binding template=\"ToastGeneric\">" +
-            "<text>" + escapedTitle + "</text>" +
-            "<text>" + escapedBody + "</text>" +
-            "</binding></visual></toast>')\n" +
-            "[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('TaskApp').Show(" +
-            "[Windows.UI.Notifications.ToastNotification]::new($x))";
-
-        var encoded = Convert.ToBase64String(Encoding.Unicode.GetBytes(script));
-
-        var process = Process.Start(new ProcessStartInfo
-        {
-            FileName = "powershell.exe",
-            Arguments = $"-NoProfile -NonInteractive -WindowStyle Hidden -EncodedCommand {encoded}",
-            UseShellExecute = false,
-            CreateNoWindow = true
-        });
-        process?.Dispose();
     }
 }
